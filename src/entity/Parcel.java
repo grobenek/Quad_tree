255a42ff1065e41f92a7873807696936eb7137ed
Peter Szathmary <peter.szathmary01@gmail.com>
2023-10-27 11:05:57 +0200

    Parcel methods implemented

--- src/entity/Parcel.java
+++ src/entity/Parcel.java
@@ -1,12 +1,68 @@
 package entity;
 
-import entity.shape.GpsCoordinates;
+import entity.shape.Rectangle;
+import quadtree.IShapeData;
 
 import java.util.List;
 
-public class Parcel {
+public class Parcel implements IShapeData {
   private int parcelNumber;
   private String description;
   private List<Property> properties;
-  private GpsCoordinates[] gpsCoordinates;
+  private Rectangle shape;
+
+  public Parcel(int parcelNumber, String description, List<Property> properties, Rectangle shape) {
+    this.parcelNumber = parcelNumber;
+    this.description = description;
+    this.properties = properties;
+    this.shape = shape;
+  }
+
+  public Parcel(int parcelNumber, String description, Rectangle shape) {
+    this.parcelNumber = parcelNumber;
+    this.description = description;
+    this.shape = shape;
+  }
+
+  public int getParcelNumber() {
+    return parcelNumber;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public List<Property> getProperties() {
+    return properties;
+  }
+
+  @Override
+  public Rectangle getShapeOfData() {
+    return shape;
+  }
+
+  @Override
+  public String toString() {
+    return "Parcel{" +
+            "parcelNumber=" + parcelNumber +
+            ", description='" + description + '\'' +
+            ", properties=" + properties +
+            ", shape=" + shape +
+            '}';
+  }
+
+   @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Parcel)) {
+      return false;
+    }
+    Parcel castedObj = (Parcel) obj;
+
+    return (castedObj.description.equals(description)
+            && ((castedObj.properties == null && properties == null)
+                || (castedObj.properties != null && castedObj.properties.equals(properties)))
+            && castedObj.parcelNumber == parcelNumber
+            && (castedObj.shape == null && shape == null)
+        || (castedObj.shape != null && castedObj.shape.equals(shape)));
+  }
 }
